29ef7928d92b72a062b868657c15f79ffef24475
Felipe Batista <felipe.batistaa52@gmail.com>
2022-05-11 22:11:40 -0300

    feat: implements basic square

--- index.js
+++ index.js
@@ -1,25 +1,130 @@
+/**
+ * let img = document.getElementById("imgSource");
+    ctx.drawImage(img, 50, 50);
+ */
+// tsc -w no terminal para watch
+var GameState;
+(function (GameState) {
+    GameState[GameState["playing"] = 0] = "playing";
+    GameState[GameState["lose"] = 1] = "lose";
+    GameState[GameState["ui"] = 2] = "ui";
+})(GameState || (GameState = {}));
 class Player {
-    constructor() {
-        this.forca_pulo = 23.6;
+    constructor(ctx, control) {
+        this.jump_force = 23.6;
+        this.jump_count = 0;
+        this.gravity = 1.6;
+        this.speed = 0;
+        this.ctx = ctx;
+        this.x = 50;
+        this.y = 0;
+        this.width = 50;
+        this.height = 50;
+        this.controls = control;
     }
-    atualiza() {
-        throw new Error("Method not implemented.");
+    update() {
+        if (this.controls.states.forward)
+            this.jump();
+        if (this.controls.states.left)
+            this.invert();
+        this.speed += this.gravity;
+        this.y += this.speed;
+        if (this.y > 250) {
+            this.y = 250; //- this.height
+            this.jump_count = 0;
+        }
     }
-    desenha() {
-        throw new Error("Method not implemented.");
+    draw() {
+        this.ctx.fillStyle = "#FA43D6";
+        this.ctx.fillRect(this.x, this.y, this.height, this.width);
     }
-    pula() {
+    jump() {
+        this.speed = -this.jump_force * (this.direction ? -1 : 1);
     }
-    inverte() {
+    invert() {
+        this.direction = !this.direction;
     }
-    atualizaBloco() { }
 }
-class Blocks {
+class Block {
+    update() {
+    }
+    draw() {
+    }
+}
+class ManipulateFile {
+    createBlock() {
+        return new Block();
+    }
+}
+class Controls {
+    constructor() {
+        this.codes = { 37: 'left', 38: 'forward', 40: 'backward' };
+        this.states = { 'left': false, 'forward': false, 'backward': false };
+        document.addEventListener('keydown', this.onKey.bind(this, true), false);
+        document.addEventListener('keyup', this.onKey.bind(this, false), false);
+    }
+    onKey(val, e) {
+        var state = this.codes[e.keyCode];
+        if (typeof state === 'undefined')
+            return;
+        this.states[state] = val;
+        e.preventDefault && e.preventDefault();
+        e.stopPropagation && e.stopPropagation();
+    }
 }
 class Game {
     constructor() {
+        this.canvas = document.createElement("canvas");
+        this.canvas.width = 500;
+        this.canvas.height = 500;
+        this.canvas.style.border = "1px solid #000";
+        this.ctx = this.canvas.getContext("2d");
+        document.body.appendChild(this.canvas);
     }
+    set player(pl) {
+        this._player = pl;
+    }
+    update() {
+        this.updateMap();
+        this._player.update();
+        /*this.blocks.forEach(element => {
+            element.update()
+        });*/
+    }
+    // manipular arquivo
+    updateMap() {
+    }
+    draw_background() {
+        this.ctx.fillStyle = "#101EF2";
+        this.ctx.fillRect(0, 0, 500, 500);
+    }
+    draw() {
+        this.draw_background();
+        this._player.draw();
+        /*this.blocks.forEach(element => {
+            element.draw()
+        });*/
+    }
+    configureScreen() {
+    }
+    // todo: pegar baseado no código de raycasting
+    configureInput() { }
+    getInput() { }
     main() {
+        this.configureScreen();
+        this.configureInput();
+        this.run();
+    }
+    run() {
+        this.update();
+        this.draw();
+        // cria o loop
+        window.requestAnimationFrame(() => this.run());
+        // images
+        this.getInput();
     }
 }
+let gm = new Game();
+gm.player = new Player(gm.ctx, new Controls());
+gm.main();
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@
  * let img = document.getElementById("imgSource");
     ctx.drawImage(img, 50, 50);
  */
+// tsc -w no terminal para watch
+
+
 
 interface IDrawable{
     update() : void;
@@ -20,33 +23,54 @@ class Player implements IDrawable{
     height : number;
     width : number;
     direction : boolean;
+    controls : Controls;
     jump_force = 23.6;
+    jump_count = 0;
+    gravity = 1.6;
+    speed = 0;
+    ctx : CanvasRenderingContext2D;
 
-    constructor(){
-
+    constructor(ctx : CanvasRenderingContext2D, control : Controls){
+        this.ctx = ctx
+        this.x =50
+        this.y = 0
+        this.width = 50
+        this.height = 50
+        this.controls = control
     }
     update() : void {
-        throw new Error("Method not implemented.");
+        if (this.controls.states.forward) this.jump();
+        if (this.controls.states.left) this.invert();
+        this.speed += this.gravity
+        this.y += this.speed
+        
+        if(this.y > 250 ){
+            this.y = 250 //- this.height
+            this.jump_count = 0
+        }
     }
     draw() : void {
-        throw new Error("Method not implemented.");
+        this.ctx.fillStyle = "#FA43D6"
+        this.ctx.fillRect(this.x,this.y, this.height, this.width)
     }
 
     public jump(){
 
+        this.speed = -this.jump_force *  (this.direction? -1: 1)
+        
     }
 
     private invert(){
-
+        this.direction = !this.direction;
     }
 }
 
 class Block implements IDrawable{
     update(): void {
-        throw new Error("Method not implemented.");
+        
     }
     draw(): void {
-        throw new Error("Method not implemented.");
+        
     }
 
 }
@@ -58,36 +82,72 @@ class ManipulateFile
     }
 }
 
+class Controls{
+    codes : any;
+    public states : any;
+
+    constructor(){
+        this.codes  = { 37: 'left', 38: 'forward', 40: 'backward' };
+        this.states = { 'left': false, 'forward': false, 'backward': false };
+        document.addEventListener('keydown', this.onKey.bind(this, true), false);
+        document.addEventListener('keyup', this.onKey.bind(this, false), false);
+    }
+
+    onKey(val : Function, e : any){
+        var state = this.codes[e.keyCode];
+        if (typeof state === 'undefined') return;
+        this.states[state] = val;
+        e.preventDefault && e.preventDefault();
+        e.stopPropagation && e.stopPropagation();
+    }
+}
+
 class Game implements IDrawable{
     state : GameState;
-    player : Player;
+    private _player : Player;
     blocks : Block[];
     canvas : HTMLCanvasElement;
     ctx : CanvasRenderingContext2D;
 
     constructor(){
         this.canvas = document.createElement("canvas");
+        
+        this.canvas.width = 500
+        this.canvas.height = 500
+        this.canvas.style.border = "1px solid #000";
+        
         this.ctx = this.canvas.getContext("2d")
         document.body.appendChild(this.canvas)
     }
     
+    public set player(pl : Player) {
+        this._player = pl;
+        
+    } 
+    
     update() : void {
         this.updateMap();
-        this.player.update();
-        this.blocks.forEach(element => {
+        this._player.update();
+        /*this.blocks.forEach(element => {
             element.update()
-        });
+        });*/
     }
     // manipular arquivo
     updateMap() : void{
 
     }
 
+    private draw_background() : void {
+        this.ctx.fillStyle =  "#101EF2"
+        this.ctx.fillRect(0, 0, 500, 500)
+    }
+
     draw() : void {
-        this.player.draw()
-        this.blocks.forEach(element => {
+        this.draw_background()
+        this._player.draw()
+        /*this.blocks.forEach(element => {
             element.draw()
-        });
+        });*/
     }
 
     private configureScreen() : void{
@@ -104,12 +164,21 @@ class Game implements IDrawable{
         this.configureInput();
         this.run();
     }
-    public run( ) : void
+
+    public run() : void
     {
         this.update();
-
+        this.draw();
+        
+        // cria o loop
+        window.requestAnimationFrame(() => this.run())
         // images
         this.getInput();
-        this.draw();
     }
 }
+
+let gm = new Game();
+
+gm.player = new Player(gm.ctx, new Controls());
+
+gm.main()
